c7f9160bf61cde63b4d8c7215e99e9ee7a2fd018
ychistel <ychistel@ac-caen.fr>
2023-09-15 16:50:40 +0200

    Modification fichier conf.py

--- source/conf.py
+++ source/conf.py
@@ -57,17 +57,17 @@ html_theme = 'sphinx_book_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../../_static']
+html_static_path = ['_static']
 html_title = 'NSI'
 
-html_css_files = ['css/custom.css']
+html_css_files = ['custom.css']
 
 """
 html_theme_options = {
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
-    'navigation_depth': 4,
+    'navigation_depth': 1,
     'includehidden': True,
     'titles_only': False
 }
